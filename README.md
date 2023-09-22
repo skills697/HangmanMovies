62876237cc0466241abf1ad0333974e826bb7173
Patrick Howe <skills697@gmail.com>
2023-09-21 22:25:41 -0500

    Changes to fix new game action
    Signed-off-by: Patrick Howe <skills697@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# HangmanMovies
-Hangman game with IMDB's Top Movies - Made with Node/Express/Angular?SQLite
+Hangman game with IMDB's Top Movies - Made with Node/Express/Angular/SQLite
+
+Once files have been downloaded, run the npm install for loading the node modules
+
+Run the hangman services with "npm run serve"
+
+Currently setup to run on localhost:4200
--- src/app/key-button/key-button.component.ts
+++ src/app/key-button/key-button.component.ts
@@ -1,13 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-key-button',
   templateUrl: './key-button.component.html',
   styleUrls: ['./key-button.component.scss']
 })
-export class KeyButtonComponent {
+export class KeyButtonComponent implements OnChanges {
 
   @Input() keyVal: string | undefined;
   @Input() stopped = false;
+  @Input() gameCounter = 0;
   @Output() keySelect = new EventEmitter<string>();
   pressed = false;
   
@@ -15,6 +16,24 @@ export class KeyButtonComponent {
     if(!this.pressed && !this.stopped){
       this.pressed = true;
       this.keySelect.emit("" + this.keyVal);
+    }
+  }
+  
+  ngOnChanges(changes: SimpleChanges){
+    console.log("OnChanges Firing");
+    let stchange = changes['stopped'];
+    console.log("stchange=" + stchange);
+    if(stchange && stchange.previousValue != undefined){
+      if(stchange.previousValue && stchange.currentValue == false){
+        console.log("Enabling Button for Stopped");
+        this.pressed = false;
+      }
+    }
+    let gc = changes['gameCounter'];
+    console.log("gc=" + gc);
+    if(gc && gc.previousValue != undefined){
+      console.log("Enabling Button for GC");
+      this.pressed = false;
     }
   }
 }
